6bb18d8dacbd9ac8aeffbad151e6450a5bcea4e2
Roman Telik <rtelik58@gmail.com>
2023-08-27 21:04:44 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Header } from './Header';
 import { routes } from 'routes';
 import { Home } from 'pages/Home';
@@ -8,25 +8,25 @@ import { MovieDetails } from 'pages/MovieDetails';
 
 export const App = () => {
   return (
-    <BrowserRouter basename="/goit-react-hw-05-movies">
-      <div
-        style={{
-          // height: '100vh',
-          display: 'flex',
-          // justifyContent: 'center',
-          // alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Routes>
-          <Route path={routes.HOME} element={<Header />}>
-            <Route index element={<Home />} />
-            <Route path={routes.MOVIES} element={<Movies />} />
-            <Route path={routes.MOVIE_ID} element={<MovieDetails />} />
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    // <BrowserRouter basename="/goit-react-hw-05-movies">
+    <div
+      style={{
+        // height: '100vh',
+        display: 'flex',
+        // justifyContent: 'center',
+        // alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Routes>
+        <Route path={routes.HOME} element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path={routes.MOVIES} element={<Movies />} />
+          <Route path={routes.MOVIE_ID} element={<MovieDetails />} />
+        </Route>
+      </Routes>
+    </div>
+    // </BrowserRouter>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { routes } from 'routes';
 // import styled from 'styled-components';
 
 export const Header = () => {
   return (
-    <header>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor: '#e3e3e3',
-          // position: 'fixed',
-          // top: '0',
-          width: '100%',
-          height: '50px',
-        }}
-      >
-        <nav>
-          <Link to={routes.HOME}>Home</Link>
-          <Link to={routes.MOVIES}>Movies</Link>
-          {/* <p>ndfjgxjnjs</p> */}
-        </nav>
-      </div>
-    </header>
+    <div>
+      <header>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#e3e3e3',
+            // position: 'fixed',
+            // top: '0',
+            width: '100%',
+            height: '50px',
+          }}
+        >
+          <nav>
+            <Link to={routes.HOME}>Home</Link>
+            <Link to={routes.MOVIES}>Movies</Link>
+            {/* <p>ndfjgxjnjs</p> */}
+          </nav>
+        </div>
+      </header>
+      <Outlet />
+    </div>
   );
 };
