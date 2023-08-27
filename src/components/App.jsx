d239d74f423c79e2ea0265c545b95011e3e02a44
Roman Telik <rtelik58@gmail.com>
2023-08-27 14:56:22 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,42 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Header } from './Header';
+import { routes } from 'routes';
+import { Home, MovieDetails, Movies } from 'pages';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
+    <BrowserRouter>
       <div
         style={{
+          height: '100vh',
           display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
-          backgroundColor: '#e3e3e3',
-          position: 'fixed',
-          top: '0',
-          width: '100%',
-          height: '50px',
+          fontSize: 40,
+          color: '#010101',
         }}
       >
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/movies">Movies</Link>
-        </nav>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#e3e3e3',
+            position: 'fixed',
+            top: '0',
+            width: '100%',
+            height: '50px',
+          }}
+        >
+          <Routes>
+            <Route path={routes.HOME} element={<Header />}>
+              <Route index element={<Home />} />
+              <Route path={routes.MOVIES} element={<Movies />} />
+              <Route path={routes.MOVIE_ID} element={<MovieDetails />} />
+            </Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+export const routes = {
+  HOME: '/',
+  MOVIES: '/movies',
+  MOVIE_ID: '/movies/:movieId',
+  CAST: '/movies/:movieId/cast',
+  REVIEVS: '/movies/:movieId/reviews',
+};
